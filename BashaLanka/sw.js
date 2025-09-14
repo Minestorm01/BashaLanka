71eca34ce6aacc980283259f488ddedc25c3cc3a
Minestorm01 <112302130+Minestorm01@users.noreply.github.com>
2025-09-14 11:50:20 +1000

    Delete BashaLanka directory

--- BashaLanka/sw.js
+++
@@ -1,3 +0,0 @@
-self.addEventListener('install', e => { self.skipWaiting(); });
-self.addEventListener('activate', e => { e.waitUntil(clients.claim()); });
-// Caching can be added later for offline support.
